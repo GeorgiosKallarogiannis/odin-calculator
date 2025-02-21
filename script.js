5a76b6ebb6f207f8097cb8e5a2b48a0aa5ec330d
GeorgiosKallarogiannis <g.kallarogiannis@its-stuttgart.de>
2025-02-21 22:06:50 +0100

    Add multiply function and function test
    This is needed to calculate the product of two
    numbers. The test was added to check the return
    value of the function.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function subtract(firstNumber, secondNumber) {
     return firstNumber - secondNumber;
 }
 
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
 if (typeof module === "object") {
-    module.exports = { add, subtract };
+    module.exports = { add, subtract, multiply };
 }
--- script.spec.js
+++ script.spec.js
@@ -1,4 +1,4 @@
-const { add, subtract } = require("./script");
+const { add, subtract, multiply } = require("./script");
 
 test("adds 1 + 2 to equal 3", () => {
     expect(add(1, 2)).toBe(3);
@@ -10,5 +10,17 @@ describe("subtract", () => {
     });
     test("subtract 1 - 2 to equal -1", () => {
         expect(subtract(1, 2)).toBe(-1);
+    });
+});
+
+describe("multiply", () => {
+    test("multiply 2 * 2 to equal 4", () => {
+        expect(multiply(2, 2)).toBe(4);
+    });
+    test("multiply 2 * -2 to equal -4", () => {
+        expect(multiply(2, -2)).toBe(-4);
+    });
+    test("multiply 2 * 0 to equal 0", () => {
+        expect(multiply(2, 0)).toBe(0);
     });
 });
